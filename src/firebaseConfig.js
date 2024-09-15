70aa2ab55cc173d6e8c01d24c717b77d67a12cf6
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-09-15 13:52:08 +0530

    Refactor WGaccount component, add user info display, and implement profile editing functionality

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,37 +1,35 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { addDoc } from "firebase/firestore";
-import { collection } from "firebase/firestore";
-import { query } from "firebase/firestore";
-import { where } from "firebase/firestore";
-import { getDocs } from "firebase/firestore";
-import { doc } from "firebase/firestore";
-import { getDoc } from "firebase/firestore";
-
-// import { getAnalytics } from "firebase/analytics";
-
+import { getFirestore, addDoc, collection, query, where, getDocs, doc, getDoc, updateDoc } from "firebase/firestore"; 
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"; 
 const firebaseConfig = {
-        apiKey: "AIzaSyA5wovpQGmQf7y4eItnD8h_xtk28jRZUSc",
-        authDomain: "profile2career-cf13a.firebaseapp.com",
-        projectId: "profile2career-cf13a",
-        storageBucket: "profile2career-cf13a.appspot.com",
-        messagingSenderId: "123739242200",
-        appId: "1:123739242200:web:487a7cbecb8c378f3721ad",
-        measurementId: "G-7X3GKTLWS5"
-  };
-
+    apiKey: "AIzaSyA5wovpQGmQf7y4eItnD8h_xtk28jRZUSc",
+    authDomain: "profile2career-cf13a.firebaseapp.com",
+    projectId: "profile2career-cf13a",
+    storageBucket: "profile2career-cf13a.appspot.com",
+    messagingSenderId: "123739242200",
+    appId: "1:123739242200:web:487a7cbecb8c378f3721ad",
+    measurementId: "G-7X3GKTLWS5"
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-// const analytics = getAnalytics(app);
-export { auth, provider, db };
-export { addDoc };
-export { collection };
-export { query };
-export { where };
-export { getDocs };
-export { doc };
-export { getDoc };
+const storage = getStorage(app); 
+export {
+    auth,
+    provider,
+    db,
+    storage,
+    addDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    doc,
+    getDoc,
+    updateDoc, // Added updateDoc to the exports
+    ref,
+    uploadBytes,
+    getDownloadURL
+};
